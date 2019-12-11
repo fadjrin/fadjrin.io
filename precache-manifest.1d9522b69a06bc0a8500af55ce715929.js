584d8af1d6a4a80cd7c88f403c1f7519f9b16165
Fadjrin Kurnia <fadjrin@gmail.com>
2019-12-11 14:30:06 +0700

    Updates

---
+++ precache-manifest.1d9522b69a06bc0a8500af55ce715929.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c4ccae4275ce677ca503959ddb4ead",
+    "url": "/fadjrin.io/index.html"
+  },
+  {
+    "revision": "a5f5d1733ab2432b1e5a",
+    "url": "/fadjrin.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ec1dc64f5bb81affca2f",
+    "url": "/fadjrin.io/static/js/2.dad81eb6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fadjrin.io/static/js/2.dad81eb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5f5d1733ab2432b1e5a",
+    "url": "/fadjrin.io/static/js/main.75286034.chunk.js"
+  },
+  {
+    "revision": "b92239c965eb435a28b9",
+    "url": "/fadjrin.io/static/js/runtime-main.534c49df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fadjrin.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
